6dc34753be736d1d2c078fc67c14815746af97fa
skrijwan <skrijwan329@gmail.com>
2025-03-09 12:18:40 +0530

    add fs module and create upload directory if it doesn't exist

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,5 +1,6 @@
 const multer= require('multer');
 const path = require("path");
+const fs= require("fs")
 const uploadDir = path.join(__dirname, "../upload");
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
