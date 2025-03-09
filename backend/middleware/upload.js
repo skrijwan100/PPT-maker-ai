fbe4aedce150b10873eb147e94ef42c9595847f9
skrijwan <skrijwan329@gmail.com>
2025-03-09 12:15:02 +0530

    add fs dir

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,9 +1,12 @@
 const multer= require('multer');
 const path = require("path");
-
+const uploadDir = path.join(__dirname, "../upload");
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "upload/"); // Store images in 'uploads' folder
+        cb(null, uploadDir); // Store images in 'uploads' folder
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
