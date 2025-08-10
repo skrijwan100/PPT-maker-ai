869e19dc34e315de3c9815976424fc84571aa87b
skrijwan <skrijwan329@gmail.com>
2025-08-10 19:55:16 +0530

    a

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,7 +10,6 @@ export default function About({showAlert}) {
      This web app use Gemini ai. This is now not ready yet. some useful and new update are comeing soon. If anyone want  contribute go to the repo.
       </p> */}
       <div style={{display:"flex",alignItems:"center",justifyContent:"center",marginTop:"30px"}}>
-
      <a href="https://github.com/skrijwan100/PPT-maker-ai" target='_blank'><button className='loginbtn' style={{ height: "40px", width: "100px", borderRadius: "10px", outline: "none",border:"none", backgroundColor: "white", cursor: "pointer", color: "#680ce7" }}>Source code</button></a>
       </div>
     </div>
